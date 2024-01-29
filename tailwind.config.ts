a4a9c9d524d12ba8892d6ee784b97411e30320ca
Filip Maszota <filipmaszota3@gmail.com>
2024-01-29 18:26:38 +0100

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1536px",
+        desktop: "1720px",
+        ultra: "2560px",
+      },
+      colors: {
+        primary:"#023F3A",
+        navbar: "#584F49",
+        text:"#363636",
+        block:"#EBF0EF"
+      },
+      fontFamily: {
+        poppins: ["Poppins","sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
